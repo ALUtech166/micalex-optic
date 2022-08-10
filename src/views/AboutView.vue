d9950a93d92260bdb9541fc958dce6119eb8966c
ALUtech166 <s.tassiga@alustudent.com>
2022-08-10 09:35:41 +0000

    container

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -30,7 +30,7 @@
     </div>
 
     <section class="objectif">
-      <div class="container">
+      <div class="">
         <div class="banner">
           <h1>Notre Vision et Objectif</h1>
         </div>
